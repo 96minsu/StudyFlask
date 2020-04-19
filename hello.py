b80638eb0634807dd291e271d8ce8424c871bf9a
MINSU-LEE <minsu960908@gmail.com>
2020-04-19 15:31:10 +0900

    서버 구동

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+# flask를 구동시키기 위한 어플리케이션 인스턴스를 생성
+from flask import Flask
+app = Flask(__name__)
+print(app)
+
+# hello world 출력하기
+@app.route( '/' )
+def hello():
+    return 'Hello World !'
+
+# flask를 구동시키기 위한 어플리케이션 인스턴스를 run()을 구동해서 시작
+if __name__ == '__main__':
+    app.run(debug=True)
