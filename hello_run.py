36a4bb20a769f22199dd5878d5fee9cb53a02aa8
MINSU-LEE <minsu960908@gmail.com>
2020-04-19 15:51:30 +0900

    어플리케이션 컨택스트

---
+++ hello_run.py
@@ -0,0 +1,10 @@
+# writefile flaskr/hello_run.py
+# Current_app으로 현재 구동되는 어플리케이션 확인할 수 있음
+from hello4 import app as app4
+from hello2 import app as app2
+from flask import current_app
+
+app_ctx = app4.app_context()
+app_ctx.push()
+print(current_app.name)
+app4.run()
